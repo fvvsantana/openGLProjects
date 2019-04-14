1c6e24ce453ab3f23fa84cbbbb37b1185242d5f1
Flavio Santana <fvvsantana@gmail.com>
2019-04-14 00:18:28 -0300

    Implement and test translation, rotation and scale

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,9 +3,17 @@
 
 #include <string>
 
+namespace ml{
+    template<class T>
+        class matrix;
+}
+
 namespace utils{
     //copy file content to outputString
     bool fileToString(char* filePath, std::string &outputString);
+    ml::matrix<float> translate(ml::matrix<float> &modelMatrix, ml::matrix<float> &position);
+    ml::matrix<float> rotateZ(ml::matrix<float> &modelMatrix, float angle);
+    ml::matrix<float> scale(ml::matrix<float> &modelMatrix, ml::matrix<float> &scale);
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,99 @@
-#include <graphicslib.hpp>
 #include <iostream>
+
+/*
+#include <graphicslib.hpp>
+*/
+#include <matrixlib.hpp>
+#include <utils.hpp>
+
+
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 600
+#define ROWS 2
+#define COLS 2
 
 using namespace std;
 
+void translationTest(){
+    ml::matrix<float> position(1.f, 3, 1);
+    ml::matrix<float> identityMatrix(4, 4, true);
+
+    ml::matrix<float> translationMatrix = utils::translate(identityMatrix, position);
+
+    cout << "position:" << endl << position << endl;
+    cout << "identity:" << endl << identityMatrix << endl;
+    cout << "translation matrix:" << endl << translationMatrix << endl;
+}
+
+void rotationTest(){
+    float angle = 3.1415/6.f;
+    ml::matrix<float> identityMatrix(4, 4, true);
+
+    ml::matrix<float> rotationMatrix = utils::rotateZ(identityMatrix, angle);
+
+    cout << "angle:" << endl << angle << endl;
+    cout << "identity:" << endl << identityMatrix << endl;
+    cout << "rotation matrix:" << endl << rotationMatrix << endl;
+}
+
+void scaleTest(){
+    ml::matrix<float> scale(2.f, 3, 1);
+    ml::matrix<float> identityMatrix(4, 4, true);
+
+    ml::matrix<float> scaleMatrix = utils::scale(identityMatrix, scale);
+
+    cout << "scale:" << endl << scale << endl;
+    cout << "identity:" << endl << identityMatrix << endl;
+    cout << "scale matrix:" << endl << scaleMatrix << endl;
+}
 
 
 int main(int argc, char const *argv[]) {
+    //translationTest();
+    //scaleTest();
+    //rotationTest();
+
+
+    /*
     graphicslib::Window window(WINDOW_WIDTH, WINDOW_HEIGHT);
     window.createWindow();
     window.run();
 
-
     cout << "oi gentee" << endl;
+    */
+    /*
+
+    ml::matrix<float> m3(1.f, ROWS, COLS);
+    ml::matrix<float> m4(ROWS, COLS, true);
+    ml::matrix<float> m5(ROWS, COLS);
+    //ml::matrix<int> m5 = ml::matrix<int>(ROWS, COLS);
+    //ml::matrix<int> m6(ROWS, COLS);
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            m3[i][j] = i*ROWS+j+1;
+            m4[i][j] = 0.5;
+            m5[i][j] = 2;
+            //m6[i][j] = 2;
+        }
+    }
+
+    //matrix<int> m5(m3+m4);
+    m5 = m3 * m4;
+    //(m3 + m4) + (m5 + m6);
+    //m5 = (ml::matrix<float>)m3 + m4;
+    //m3 + m4;
+    //(ml::matrix<int>) m3 + m4;
+    //cout << (m3 + m4) << endl;
+
+    cout << "m3:" << endl << m3 << endl;
+    cout << "m4:" << endl << m4 << endl;
+    cout << "m5:" << endl << m5 << endl;
+    //cout << "m6:" << endl << m6 << endl;
+    */
+
+
+
     return 0;
 }
 
